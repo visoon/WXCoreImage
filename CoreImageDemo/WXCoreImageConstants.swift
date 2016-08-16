d244bece00c69a1aec2d46ac0a0ef981961d2cd5
vison <xiang.wang@monstar-lab.com>
2016-08-16 16:58:01 +0800

    first commit

---
+++ CoreImageDemo/WXCoreImageConstants.swift
@@ -0,0 +1,13 @@
+//
+//  WXCoreImageConstants.swift
+//  CoreImageDemo
+//
+//  Created by vison on 16/8/16.
+//  Copyright © 2016年 vison. All rights reserved.
+//
+
+import UIKit
+
+class WXCoreImageConstants: NSObject {
+
+}
---
+++ CoreImageDemo/WXCoreImageRoot.swift
@@ -0,0 +1,13 @@
+//
+//  WXCoreImageRoot.swift
+//  CoreImageDemo
+//
+//  Created by vison on 16/8/16.
+//  Copyright © 2016年 vison. All rights reserved.
+//
+
+import UIKit
+
+class WXCoreImageRoot: NSObject {
+
+}
